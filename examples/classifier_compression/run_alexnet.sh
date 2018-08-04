a2437d5b57dd74388d282fd7a10cc61322ed5aee
Dan Alistarh <d.alistarh@gmail.com>
2018-08-04 08:34:42 -0400

    changed for alexnet

--- examples/classifier_compression/run_alexnet.sh
+++ examples/classifier_compression/run_alexnet.sh
@@ -1,6 +1,6 @@
-python3.6 compress_classifier.py -a=alexnet --lr=0.005 -p=50 ~/data-disk/ILSVRC2012/ --dataset imagenet -j 22 --epochs 90 --pretrained --compress=../agp-pruning/alexnet.schedule_agp.yaml
+python3.6 compress_classifier.py -a=alexnet --lr=0.005 -p=50 ~/data-disk/ILSVRC2012/ --dataset imagenet -j 22 --epochs 90 --pretrained --compress=../agp-pruning/alexnet.schedule_agp.yaml --name="alexnet_80_conv_90_fc"
 
 
-python3.6 compress_classifier.py -a=alexnet --teacher_arch alexnet --teacher_weights /home/dan/.torch/models/alexnet-owt-4df8aa71.pth --lr=0.005 -p=50 ~/data-disk/ILSVRC2012/ --dataset imagenet -j 22 --epochs 90 --pretrained --compress=../agp-pruning/alexnet.schedule_agp.yaml
+python3.6 compress_classifier.py -a=alexnet --teacher_arch alexnet --teacher_weights /home/dan/.torch/models/alexnet-owt-4df8aa71.pth --lr=0.005 -p=50 ~/data-disk/ILSVRC2012/ --dataset imagenet -j 22 --epochs 90 --pretrained --compress=../agp-pruning/alexnet.schedule_agp.yaml --name="alexnet_80_conv_90_fc_distilled_init"
 
 
