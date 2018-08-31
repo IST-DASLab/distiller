bd0b1de32e8b90405867b1b5ef494fb811e2a03f
Antonio <antspy@users.noreply.github.com>
2018-08-31 19:40:19 +0200

    Added fix for decreasing sparsity

--- distiller/pruning/level_pruner.py
+++ distiller/pruning/level_pruner.py
@@ -17,6 +17,38 @@
 import torch
 from .pruner import _ParameterPruner
 import distiller
+import random
+
+def unmask_values_random(mask, number_to_unmask):
+
+    '''
+    :param mask: A 1d tensor consisting only of zeros and ones
+    :param number_to_unmask: How many zeros to flip to one; will be chosen uniformly at random
+    :return: The modified mask
+
+    This method will implement reservoir sampling to achieve uniform sampling in unmasking the zeros.
+    '''
+
+    if number_to_unmask <= 0: return mask
+
+    indices_unmasked_values = []
+    count_zero_seen = 0
+    for idx, value in enumerate(mask):
+        if value != 0: continue
+        count_zero_seen += 1
+        if len(indices_unmasked_values) < number_to_unmask:
+            indices_unmasked_values.append(idx)
+            continue
+        if random.random() <= number_to_unmask/count_zero_seen:
+            #keep new item and replace old one
+            rand_idx = random.randint(0, number_to_unmask-1) #is this reliable for big number_to_unmask?
+            indices_unmasked_values[rand_idx] = idx
+        else: pass #keep old items, discard new one
+
+    for idx in indices_unmasked_values:
+        mask[idx] = 1
+
+    return mask
 
 class SparsityLevelParameterPruner(_ParameterPruner):
     """Prune to an exact pruning level specification.
@@ -45,6 +77,25 @@ class SparsityLevelParameterPruner(_ParameterPruner):
 
     @staticmethod
     def prune_level(param, param_name, zeros_mask_dict, desired_sparsity):
-        bottomk, _ = torch.topk(param.abs().view(-1), int(desired_sparsity * param.numel()), largest=False, sorted=True)
-        threshold = bottomk.data[-1] # This is the largest element from the group of elements that we prune away
-        zeros_mask_dict[param_name].mask = distiller.threshold_mask(param.data, threshold)
+        expected_number_pruned_values = int(desired_sparsity * param.numel())
+        #No need to sort, just take min later - it's O(k) vs O(k log k)
+        bottomk, _ = torch.topk(param.abs().view(-1), expected_number_pruned_values)
+        threshold = bottomk.min() # This is the largest element from the group of elements that we prune away
+        parameter_weight_mask = distiller.threshold_mask(param.data, threshold)
+
+        # The percentage of pruned weights now it's larger or equal than the desired sparsity. We would like it to make
+        # sure that it's actually equal, otherwise it will enforce a lot more weights to be zero than expected
+        number_pruned_values = (parameter_weight_mask==0).int().sum()
+
+        if number_pruned_values < expected_number_pruned_values:
+            raise ValueError('Number of pruned value should be higher than the desired sparsity')
+
+        # We will unmask values at random so to ensure that the final mask has the desired sparsity
+        number_values_to_unmask = number_pruned_values-expected_number_pruned_values
+        parameter_weight_mask = unmask_values_random(parameter_weight_mask.view(-1), number_values_to_unmask)
+        zeros_mask_dict[param_name].mask = parameter_weight_mask.view_as(param.data)
+
+        # some debug information - can delete after code works correctly
+        print('Expected percentage of pruned values: {}'.format(desired_sparsity))
+        print('Actual percentage before fix: {}'.format(number_pruned_values / param.numel()))
+        print('Actual percentage after fix: {}'.format((parameter_weight_mask==0).int().sum() / param.numel()))
